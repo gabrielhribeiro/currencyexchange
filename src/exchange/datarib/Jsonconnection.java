9dfcccf4c7901d47a2c96c3d776071945f65a30a
gabrielhribeiro <gabrielrdavid@gmail.com>
2020-05-26 22:36:05 -0300

    json to open request from API

---
+++ src/exchange/datarib/Jsonconnection.java
@@ -0,0 +1,77 @@
+package exchange.datarib;
+
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Jsonconnection {
+    private String Url;
+    private String Result;
+    private static JSONObject rates;
+    private static String date;
+    private static String base;
+
+    public Jsonconnection(String Url, String Result) {
+        this.Url = Url;
+        this.Result = Result;
+    }
+
+    public Jsonconnection(String Url) {
+        this.Url = Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+
+    public void setResult(String result) {
+        Result = result;
+    }
+
+    public void setRates(JSONObject rates) {
+        this.rates = rates;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public JSONObject getRates() {
+        return rates;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    static void ClientRequest(String Url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(Url)).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(Jsonconnection::ClientDeserialize)
+                .join();
+    }
+
+    public static String ClientDeserialize(String Result){
+        JSONObject json = new JSONObject(Result);
+        Jsonconnection.rates = json.getJSONObject("rates");
+        Jsonconnection.date = json.getString("date");
+        Jsonconnection.base = json.getString("base");
+        return Result;
+    }
+}
---
+++ src/exchange/datarib/Main.java
@@ -0,0 +1,12 @@
+package exchange.datarib;
+
+public class Main {
+    public static void main(String[] args) {
+        Jsonconnection requests = new Jsonconnection("default");
+        requests.ClientRequest("https://api.exchangeratesapi.io/latest?base=BRL");
+        System.out.println(requests.getDate());
+        System.out.println(requests.getRates());
+        System.out.println(requests.getBase());
+
+    }
+}
