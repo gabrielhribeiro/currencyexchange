79f593737f5eb9551c855d40fa37c5261cd0a150
gabrielhribeiro <gabrielrdavid@gmail.com>
2020-05-30 22:15:38 -0300

    loop

--- src/exchange/datarib/Main.java
+++ src/exchange/datarib/Main.java
@@ -1,14 +1,22 @@
 package exchange.datarib;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Set;
+
+import static com.sun.tools.javac.util.Constants.format;
 
 public class Main {
     public static void main(String[] args) {
         Jsonconnection requests = new Jsonconnection("default");
-        requests.ClientRequest("https://api.exchangeratesapi.io/latest?base=EUR");
-        //System.out.println(requests.getDate());
-        //System.out.println(requests.getRates());
-        // System.out.println(requests.getBase());
+        requests.ClientRequest("https://api.exchangeratesapi.io/latest");
+        System.out.println(requests.getDate());
+        System.out.println(requests.getRates());
+        System.out.println(requests.getBase());
         //requests.getRates().keySet().forEach(System.out::println);
         // String[] arrData = requests.getRates().keySet();
 
@@ -16,27 +24,23 @@ public class Main {
       //  System.out.println(requests.getRates().keySet());
 
      //   Set<String> jsonArray = requests.getRates().keySet();
-
-
         Set<String> ReqVariation = requests.getRates().keySet();
-        for (String element : ReqVariation) {
-            requests.ClientRequest("https://api.exchangeratesapi.io/latest?base=" + element);
-           // System.out.println(requests.getRates().get(element));
-            System.out.println(element);
+        String e = requests.getDate();
+        String s = "2018-03-01";
+       // String e = "2020-05-01";
+        LocalDate start = LocalDate.parse(s);
+        LocalDate end = LocalDate.parse(e);
+        List<LocalDate> totalDates = new ArrayList<>();
+        while (!start.isAfter(end)) {
+            totalDates.add(start);
+            start = start.plusDays(1);
+            for (String element : ReqVariation) {
+                requests.ClientRequest("https://api.exchangeratesapi.io/"+ start +"?base=" + element);
+                // System.out.println(requests.getRates().get(element));
+                System.out.println(element);
+                System.out.println(ReqVariation);
+            }
+            System.out.println(start);
         }
-
-
-
-
-
-
-
-
-
-  //      System.out.println(jsonArray);
-  //      for (String element : jsonArray) {
-  //          System.out.println(requests.getRates().get(element));
-  //          System.out.println(element);
-  //      }
     }
 }
